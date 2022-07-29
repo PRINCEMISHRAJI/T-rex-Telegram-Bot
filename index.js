f7fb79e3bd4cf194eab049653687efa70b3b8acf
Prince Mishra <50262987+PRINCEMISHRAJI@users.noreply.github.com>
2022-07-30 02:22:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = express();
 const bot = new TelegramBot(TOKEN, { polling: true } );
 
 const port = process.env.PORT || 5000;
-const gameName = "trexrobo";
+const gameName = "Trexrobo";
 
 const queries = {};
 
