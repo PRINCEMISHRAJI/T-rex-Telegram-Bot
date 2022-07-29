7615f8d409d2e693690cac5a8302cf662dc369d2
Prince Mishra <50262987+PRINCEMISHRAJI@users.noreply.github.com>
2022-07-30 02:45:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
-const TOKEN;
+const TOKEN = "API_TOKEN";
 const server = express();
 const bot = new TelegramBot(TOKEN, { polling: true } );
 
