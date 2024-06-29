beb6b6d1244835b4a77f5c9501cc1759dfc6d813
unknown <hellcyberworld@gmail.com>
2024-06-30 00:37:54 +0530

    update url to get highscore

--- index.js
+++ index.js
@@ -8,19 +8,9 @@ const port = process.env.PORT || 5000;
 server.use(express.static(path.join(__dirname, 'public')));
 
 server.get("/highscore/:score", function (req, res, next) {
-    console.log('Highscore endpoint hit');
-    console.log('Request query:', req.query);
-    console.log('Request params:', req.params);
-
-    // Check if the query ID exists in the queries object
-    if (!Object.hasOwnProperty.call(queries, req.query.id)) {
-        console.error('Query ID not found in queries object');
-        return next();
-    }
-
+    if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
     let query = queries[req.query.id];
     let options;
-
     if (query.message) {
         options = {
             chat_id: query.message.chat.id,
@@ -31,19 +21,10 @@ server.get("/highscore/:score", function (req, res, next) {
             inline_message_id: query.inline_message_id
         };
     }
-
-    console.log('Options for setGameScore:', options);
-
-    bot.setGameScore(query.from.id, parseInt(req.params.score), options, function (err, result) {
-        if (err) {
-            console.error('Error setting game score:', err);
-            res.status(500).send('Error setting game score');
-        } else {
-            console.log('Game score set successfully:', result);
-            res.status(200).send('Game score set successfully');
-        }
-    });
+    bot.setGameScore(query.from.id, parseInt(req.params.score), options,
+        function (err, result) { });
 });
+
 
 server.listen(port, ()=> {
     console.log(`Server is running on https://localhost:${port}`)
